eaf90aea8d7ef3a2863e6064e9406b57a57f572b
rzqh <rizqih@protonmail.ch>
2024-02-14 22:32:55 +0700

    create dockerfile and pipeline file

---
+++ 01_infrastructure/01_docker_and_postgres/pipeline.py
@@ -0,0 +1,15 @@
+import sys
+import pandas
+
+
+# menampilkan argumen
+print(sys.argv)
+
+# argumen 0 adalah nama file
+# argumen 1 berisi argumen pertama yang kita perlukan
+day = sys.argv[1]
+
+# tampilkan kalimat dengan argumen
+print(f'job finished successfully for day = {day}')
+
+
